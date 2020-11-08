3e22d9e62ae13fa07a9ec7f18b1dfccd7849859b
Frank Denis <github@pureftpd.org>
2020-11-08 16:05:17 +0100

    Format trustinsoft/stub.c like the rest of the code

--- trustinsoft/stub.c
+++ trustinsoft/stub.c
@@ -5,25 +5,32 @@
    trustinsoft/common.config */
 size_t __mkfs_preallocate_size = 1000000;
 
-int rand(void) {
-  return 137;
+int
+rand(void)
+{
+    return 137;
 }
 
 /* Replace the 'SYS_getrandom' system call.
    See 'compilation_cmd' in trustinsoft/gcc_x86_64.config */
-int tis_getrandom(void * const buf_, size_t size, int flags)
+int
+tis_getrandom(void *const buf_, size_t size, int flags)
 {
-  unsigned char *buf = (unsigned char*) buf_;
+    unsigned char *buf = (unsigned char *) buf_;
 
-  for (size_t i = 0; i < size; i++)
-    *buf++ = i;
-
-  return (int)size;
+    for (size_t i = 0; i < size; i++) {
+        *buf++ = i;
+    }
+    return (int) size;
 }
 
 /* Stub the call to 'sysconf(_SC_PAGESIZE)'. */
 /*@ ensures sysconf_OK: \result != -1; */
-long sysconf(int name) {
-  if(name == _SC_PAGESIZE) return 4096;
-  return -1;
+long
+sysconf(int name)
+{
+    if (name == _SC_PAGESIZE) {
+        return 4096;
+    }
+    return -1;
 }
